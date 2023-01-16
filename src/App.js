17a4d10ba3a0a0a1cc1645e70fae00cfd3920e43
Minho Park <pminh98@naver.com>
2023-01-16 22:10:31 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([])
+  const [usd, setUsd] = useState(1);
+  const moneyFn = (event) => setUsd(event.target.value);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then(response => response.json())
+    .then(json => {
+      setCoins(json)
+      setLoading(false);
+    });
+  }, []);
+  return(
+    <div>
+      <h1>{loading ? "" : `${coins.length} Coins Price List`}</h1>
+      {loading ? <strong>Loading...</strong> 
+        : 
+          <select>
+            {coins.map((items) => (
+              <option>
+                {items.name} // {items.symbol} // {items.quotes.USD.price.toFixed(2)} USD
+              </option>
+            ))}
+          </select>}
+      <hr/>
+      <h1>{loading ? "": "Coins with my money"}</h1>
+      <label htmlFor="money">
+        <input value = {usd} onChange={moneyFn} id="money" type="number" placeholder="How much do you have?"/> USD
+      </label>
+      {loading ? <strong>Loading...</strong> 
+        : 
+          <select>
+            {coins.map((items) => (
+              <option>
+                {items.name} // {items.symbol} // {(usd/items.quotes.USD.price).toFixed(2)} coins
+              </option>
+            ))}
+          </select>}
     </div>
   );
 }
